232f6aff32aee690a3af47c390919bec917393d0
Rohan Phillips <rohan@solutions-insurance.com>
2021-03-01 06:45:21 -0600

    preparing for react

--- watersportscentral/app/controllers/application_controller.rb
+++ watersportscentral/app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
   end
 
   def authorized
-    redirect_to '/welcome' unless logged_in?
+    # redirect_to '/welcome' unless logged_in?
   end
 
   def owns_record(record)
