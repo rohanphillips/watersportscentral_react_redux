1ff2e7eda89ccca122679854ff50b85860dc24ce
rohanphillips <rohan@solutions-insurance.com>
2021-03-03 14:33:03 -0600

    Initializing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom';
-
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route
+// } from 'react-dom';
 import './App.css';
+// import NavBar from './components/NavBar'
 
-
-const App = (props) => {
-  return (
-    <Router>
-      {
-        <div>
-          <NavBar/>
-        </div>
-      }
-    </Router>
-  );
+class App extends Component {
+ 
+  componentDidMount() {
+    // console.log("componentDidMount:")
+    // console.log(this.props)
+    // this.props.fetchCats()
+  }
+ 
+  render() {
+    // console.log(this.props.catPics) // log will fire every time App renders
+    return (
+      <div className="App">
+      </div>
+    );
+  }
 }
+ 
+// const mapStateToProps = state => {
+//   return {
+//     catPics: state.cats,
+//     loading: state.loading
+//   }
+// }
+ 
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     fetchCats: () => dispatch(fetchCats())
+//   }
+// }
+export default App
 
-export default App;
+// const App = (props) => {
+//   return (
+//     <Router>
+//       {
+//         <div>
+//           {/* <NavBar/> */}
+//         </div>
+//       }
+//     </Router>
+//   );
+// }
+
+// export default App;
