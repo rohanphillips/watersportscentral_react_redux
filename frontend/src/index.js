87aee5057426d1a15231250d24879e186055f281
rohanphillips <rohan@solutions-insurance.com>
2021-03-03 14:45:58 -0600

    Initializing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
+const store = createStore(applyMiddleware(thunk))
+
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     {/* <App /> */}
   </Provider>,
   document.getElementById('root')
