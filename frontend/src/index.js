9c5b596a158a2b43574f84916789528e5ed7885f
rohanphillips <rohan@solutions-insurance.com>
2021-03-03 19:23:19 -0600

    update createStore for thunk

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
-const store = createStore(applyMiddleware(thunk))
+const store = createStore(siteReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
