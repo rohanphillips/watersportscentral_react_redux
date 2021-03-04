850e261c260d3586f8cad2926abcdbd705c61f2e
rohanphillips <rohan@solutions-insurance.com>
2021-03-03 20:52:45 -0600

    update createUser

--- frontend/src/actions/siteActions.js
+++ frontend/src/actions/siteActions.js
@@ -1,10 +1,21 @@
+import axios from 'axios';
 
 const CREATE_USER = 'CREATE_USER';
 const CREATE_USER_ERROR = 'CREATE_USER_ERROR';
+const SITE_URL = 'localhost:3001/'
+const NEW_USER_URL = SITE_URL & 'new'
 
 const createUser = newUser => async (dispatch) => {
   try {
-    dispatch({type: 'CREATE_USER'});
+    dispatch({type: 'CREATE_USER', ...newUser});
+    const response = await axios({
+      method: 'POST',
+      url: NEW_USER_URL,
+      data: {user: newUser},
+      crossdomain: true,
+    })
+    const { token } = response.data;
+    localStorage.setItem('jwt', token);
   } catch {
 
   }
