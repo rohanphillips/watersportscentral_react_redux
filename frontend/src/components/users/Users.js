55beb3b42686c13ae27b059f043d1ea5258785f1
rohanphillips <rohan@solutions-insurance.com>
2021-03-07 13:06:56 -0600

    Users List

---
+++ frontend/src/components/users/Users.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Users = ({ state}) => {
+  console.log("Users", "State:", state);
+  return (
+    <div> 
+        <h1>Users List</h1>     
+    </div>
+  );
+};
+
+export default Users;
--- frontend/src/components/users/UsersContainer.js
+++ frontend/src/components/users/UsersContainer.js
@@ -2,19 +2,23 @@ import React from 'react';
 import HasAccess from '../sessions/HasAccess'
 import {
   BrowserRouter as Router,
+  Switch,
   Route 
 } from 'react-router-dom';
-import { render } from 'react-dom';
+import Users from '../users/Users';
 
 function header() {
-  return <h1>Users</h1>
+  return <h1>Users List</h1>
 }
 
 const UsersContainer = ({ match, state}) => {
   console.log("UsersContainer", "State:", state);
   return (
     <div>
-      <HasAccess component={() => header() }/>
+      {/* <HasAccess component={() => header() }/> */} 
+      <Switch>
+        <HasAccess component={() =><Route exact path="/users" render={routerProps => <Users {...routerProps} state={state}/>}/>}/>
+      </Switch>
       
     </div>
   );
