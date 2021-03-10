a09e7f9c27944efe6dbf58ab63c33d0d98355a46
rohanphillips <rohan@solutions-insurance.com>
2021-03-09 22:19:03 -0600

    add edit button

--- frontend/src/components/users/UserFlat.js
+++ frontend/src/components/users/UserFlat.js
@@ -1,15 +1,17 @@
 import React from 'react'
+import {
+  Link,
+} from 'react-router-dom';
 
 const UserFlat = (data) => {
   console.log("UserFlat", "data", data)
   // console.log("UserFlat", "deleteUser", data.deleteUser)
   const {user} = data;
-
   return (
     <div>
       <p>
         {user.first_name} {user.last_name} 
-        <button>View</button> <button>Edit</button>  <button id={`${user.id}`} onClick={() => data.deleteUser(user.id)} >Delete</button>    
+        <Link to={`/users/${user.id}`}><button>View</button></Link> <Link to={`/users/${user.id}/edit`}><button>Edit</button></Link>  <button id={`${user.id}`} onClick={() => data.deleteUser(user.id)} >Delete</button>    
       </p>
     </div>
   )
