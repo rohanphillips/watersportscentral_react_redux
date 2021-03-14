da6da9d85ef25350c490ae9fb376fb9ce036e70f
rohanphillips <rohan@solutions-insurance.com>
2021-03-14 18:06:57 -0500

    LocationNewEdit buildout

---
+++ frontend/src/components/locations/LocationNewEdit.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class LocationEdit extends React.Component {
+  state = {
+    title: ''
+  }
+
+  render() {
+    return (
+      <div>
+       
+      </div>
+    );
+  };
+}
+ 
+export default LocationEdit;
