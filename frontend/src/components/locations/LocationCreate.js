0ccc0ed47b21fcc1e4a823feac0a7fa33e7e37cf
rohanphillips <rohan@solutions-insurance.com>
2021-03-15 15:54:39 -0500

    LocationCreate

--- frontend/src/components/locations/LocationCreate.js
+++ frontend/src/components/locations/LocationCreate.js
@@ -4,14 +4,57 @@ class LocationCreate extends Component {
   state = {
     name: '',
     description: '',
+    info: ''
 
+  }
+
+  handleChange = (e) => {    
+    let saved;
+    switch (e.target.type){
+      case 'checkbox':
+        saved = e.target.checked;
+        break;
+      default:
+        saved = e.target.value;
+    }
+    console.log("saved:", saved)
+    this.setState({
+      [e.target.name]: saved,
+    })
   }
 
   render() {
     console.log("LocationAddEdit:", "props", this.props)
     return (
-      <div>
-        <p>Location Create Page</p>
+      <div className="form-part">
+        <p>Location Create</p>
+        <form>
+          <label>
+            Location Name:
+            <input 
+              type="text"
+              name="name"
+              value={this.state.name}
+            />
+          </label>
+          <label>
+            Short Description:
+            <input 
+              type="text"
+              name="description"
+              value={this.state.description}
+            />
+          </label>
+          <label>
+            Location Info:
+            <input 
+              type="text"
+              name="info"
+              value={this.state.name}
+            />
+          </label>
+          <button>Create Location</button>
+        </form>
       </div>
     );
   };
