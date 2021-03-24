1771000d75a8a4cb3ae786fcaf7b3940ef3d4a29
rohanphillips <rohan@solutions-insurance.com>
2021-03-24 13:40:09 -0500

    console.log cleanup

--- frontend/src/actions/locationActions.js
+++ frontend/src/actions/locationActions.js
@@ -33,22 +33,18 @@ export const getLocations = () => {
 export const createLocation = (newLocation) => {      
   return async (dispatch) => {
     const header = {'Authorization': 'JWT ' + localStorage.getItem('loggedin')};
-    console.log("headers", header)
-    console.log("newLocation", newLocation);
     return fetch(LOCATIONS_URL,{
       method: 'POST',
       headers: header,
       body: newLocation,
       crossdomain: true,
     }).then(async(response) => {
-      console.log("Response Received", response)
       if(response.ok){
         return response.json()
       } else {
         return response.json().then(errors => Promise.reject(errors))
       }
     }).then((data) => {    
-      console.log("Data Available", data)
       dispatch({type: 'CREATE_LOCATION', location: data.location, locations: data.locations})
     }).catch((error) => {
       if(error.errors === undefined){
@@ -62,25 +58,19 @@ export const createLocation = (newLocation) => {
 export const updateLocation = (updateLocation) => {      
   return async (dispatch) => {
     const header = {'Authorization': 'JWT ' + localStorage.getItem('loggedin')};
-    console.log("headers", header)
-    console.log("updateLocation", updateLocation);
-    console.log("dispatch", dispatch);
     const id = updateLocation.get("location[id]")
-    console.log("id", id)
     return fetch(`${LOCATIONS_URL}/${id}`,{
       method: 'PATCH',
       headers: header,
       body: updateLocation,
       crossdomain: true,
     }).then(async(response) => {
-      console.log("Response Received", response)
       if(response.ok){
         return response.json()
       } else {
         return response.json().then(errors => Promise.reject(errors))
       }
     }).then((data) => {    
-      console.log("Data Available", data)
       dispatch({type: 'UPDATE_LOCATION', location: data.location, locations: data.locations})
     }).catch((error) => {
       if(error.errors === undefined){
@@ -91,17 +81,15 @@ export const updateLocation = (updateLocation) => {
   }
 } 
 
-const deleteLocation = (id) => async (dispatch) => {          
-  console.log("siteAction:", "deleteLocation:", deleteLocation);
+const deleteLocation = (id) => async (dispatch) => {  
   const header = {'Authorization': 'JWT ' + localStorage.getItem('loggedin')};
   try {
-    const response = await axios({
+    await axios({
       method: 'DELETE',
       url: `${LOCATIONS_URL}/${id}`,
       headers: header,
       crossdomain: true,
     })
-    console.log("deleteLocation:", "response:", response)
     dispatch({type: 'DELETE_LOCATION', id: id})
   } catch {
 
