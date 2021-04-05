23af9225b23f1e402d037d319697ca61c575af0d
rohanphillips <rohan@solutions-insurance.com>
2021-04-05 18:37:52 -0500

    Add location like button for review

--- frontend/src/components/locations/LocationFlat.js
+++ frontend/src/components/locations/LocationFlat.js
@@ -6,6 +6,16 @@ import IsOwner from '../sessions/IsOwner'
 
 const LocationFlat = (location) => {
   const {data} = location;
+  const handleLike = (b) =>{
+    console.log("handleLike", b.target.innerHTML);
+    switch (b.target.innerHTML){
+      case 'Not Liked':
+        console.log("test")
+        b.target.innerHTML = "Liked";
+        break;
+      default: b.target.innerHTML = 'Not Liked';
+    }
+  }
   return (
     <div className="flex-col p-2 my-2  border border-amber-200 w-max">
       <div>
@@ -26,6 +36,7 @@ const LocationFlat = (location) => {
         <IsOwner type="location" location={data}>
           <button className="btn-delete" id={`${data.id}`} onClick={() => location.deleteLocation(data.id)}>Delete</button>
         </IsOwner>
+        <button onClick={handleLike}>Not Liked</button>
       </div>
     </div>
   )
